1fe73f95380ef6f85f3227c1c7d363fc61545c50
Chris Lee <cleer@cmu.edu>
2010-11-24 15:35:03 -0500

    Some CSS fixes + home page linkage

--- main.rb
+++ main.rb
@@ -60,6 +60,6 @@ __END__
 			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 			})();
-		</script>
+	</script>
 </body>
 </html>
